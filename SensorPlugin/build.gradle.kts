0b999b5ba44f4075ecdbec18474c6738e23573b5
Corey Feist <swdcfeist@gmail.com>
2024-10-16 15:28:50 -0400

    re: add project files

--- SensorPlugin/build.gradle.kts
+++ SensorPlugin/build.gradle.kts
@@ -1,39 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.library)
-}
-
-android {
-    namespace = "com.magicleap.sensorplugin"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 29
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-}
-
-dependencies {
-
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    compileOnly(files("libs\\classes.jar"))
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
 }
